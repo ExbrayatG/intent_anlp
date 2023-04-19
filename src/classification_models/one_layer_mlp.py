simplified fasttext, first fix for padded averaging

src/classification_models/one_layer_mlp.py
@@ -15,15 +15,31 @@ class OneLayerMLP(nn.Module):
             nn.ReLU(),
             nn.Linear(self.hidden_dim, self.num_classes),
         )
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     def forward(self, inputs):
         # inputs is a list of tokenized utterances
         embeddings = self.embedding_layer(inputs)
 
         # Average the embeddings
-        average_embeddings = torch.mean(embeddings, dim=1)  # TODO - check this
+        # Remove padding tokens (all-zero rows) and calculate the average for each utterance
+        # TODO: Replace this by a mask
+        averaged_embeddings = []
+        for emb in embeddings:
+            non_zero_emb = list(filter(lambda x: torch.any(x != 0), emb))
+            if non_zero_emb:
+                non_zero_emb = torch.stack(non_zero_emb)
+                avg_emb = torch.mean(non_zero_emb, dim=0)
+            else:
+                avg_emb = torch.zeros(self.embedding_layer.embedding_dim).to(
+                    self.device
+                )
+            averaged_embeddings.append(avg_emb)
+
+        # Convert the list of averaged embeddings to a tensor
+        averaged_embeddings = torch.stack(averaged_embeddings)
 
         # Pass the embeddings through the MLP layer
-        final_out = self.network(average_embeddings)
+        final_out = self.network(averaged_embeddings)
 
         return final_out

src/embeddings/fasttext_layer.py
@@ -11,31 +11,13 @@ class FastTextLayer(nn.Module):
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     def forward(self, inputs):
-        # inputs is a list of utterances
-        inputs = [
-            [x.lower() for x in elt.split()] for elt in inputs
-        ]  # Simple tokenization
-        batch_size = len(inputs)
-        seq_lengths = [len(utterance) for utterance in inputs]
+        # Get embeddings for each utterance in the batch
+        embeddings = [
+            self.fasttext_vectors.get_vecs_by_tokens(utterance.split())
+            for utterance in inputs
+        ]
 
-        # Get the FastText embeddings for each token
-        embeddings = []
-        for utterance in inputs:
-            token_embeddings = []
-            for token in utterance:
-                token_embedding = self.fasttext_vectors[token]
-                token_embeddings.append(token_embedding)
-            embeddings.append(torch.stack(token_embeddings))
+        # Pad the sequences to the same length
+        embeddings = nn.utils.rnn.pad_sequence(embeddings, batch_first=True)
 
-        # Pad the embeddings to make them the same length
-        max_length = max(seq_lengths)
-        padded_embeddings = []
-        for i in range(batch_size):
-            padding_length = max_length - seq_lengths[i]
-            padding = torch.zeros(padding_length, self.embedding_dim)
-            padded_embeddings.append(torch.cat([embeddings[i], padding], dim=0))
-
-        # Stack the embeddings into a tensor
-        stacked_embeddings = torch.stack(padded_embeddings).to(self.device)
-
-        return stacked_embeddings
+        return embeddings.to(self.device)