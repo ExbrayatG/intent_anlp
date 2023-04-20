fixed embedding calculation

src/classification_models/lstm.py
@@ -18,7 +18,7 @@ class LSTM(nn.Module):
 
     def forward(self, inputs):
         # inputs is a list of tokenized utterances
-        embeddings = self.embedding_layer(inputs)
+        embeddings, mask = self.embedding_layer(inputs)
 
         # Pass the embeddings through the LSTM layer
         lstm_out, _ = self.lstm_layer(embeddings)

src/classification_models/one_layer_mlp.py
@@ -19,27 +19,23 @@ class OneLayerMLP(nn.Module):
 
     def forward(self, inputs):
         # inputs is a list of tokenized utterances
-        embeddings = self.embedding_layer(inputs)
+        embeddings, mask = self.embedding_layer(inputs)
 
         # Average the embeddings
-        # Remove padding tokens (all-zero rows) and calculate the average for each utterance
-        # TODO: Replace this by a mask
-        averaged_embeddings = []
-        for emb in embeddings:
-            non_zero_emb = list(filter(lambda x: torch.any(x != 0), emb))
-            if non_zero_emb:
-                non_zero_emb = torch.stack(non_zero_emb)
-                avg_emb = torch.mean(non_zero_emb, dim=0)
-            else:
-                avg_emb = torch.zeros(self.embedding_layer.embedding_dim).to(
-                    self.device
-                )
-            averaged_embeddings.append(avg_emb)
+        if mask is not None:
+            # Calculate the sum of embeddings
+            summed_embeddings = torch.sum(embeddings * mask.unsqueeze(-1), dim=1)
 
-        # Convert the list of averaged embeddings to a tensor
-        averaged_embeddings = torch.stack(averaged_embeddings)
+            # Calculate the number of tokens in each utterance
+            token_count = torch.sum(mask, dim=1, keepdim=True)
+
+            # Calculate the average of embeddings
+            averaged_embeddings = summed_embeddings / token_count
+        else:
+            # Convert the list of averaged embeddings to a tensor
+            averaged_embeddings = torch.mean(embeddings, dim=1)
 
         # Pass the embeddings through the MLP layer
-        final_out = self.network(averaged_embeddings)
+        final_out = self.network(averaged_embeddings.to(self.device))
 
         return final_out

src/classification_models/zero_layer_mlp.py
@@ -12,12 +12,23 @@ class ZeroLayerMLP(nn.Module):
 
     def forward(self, inputs):
         # inputs is a list of tokenized utterances
-        embeddings = self.embedding_layer(inputs)
+        embeddings, mask = self.embedding_layer(inputs)
 
         # Average the embeddings
-        average_embeddings = torch.mean(embeddings, dim=1)  # TODO - check this
+        if mask is not None:
+            # Calculate the sum of embeddings
+            summed_embeddings = torch.sum(embeddings * mask.unsqueeze(-1), dim=1)
+
+            # Calculate the number of tokens in each utterance
+            token_count = torch.sum(mask, dim=1, keepdim=True)
+
+            # Calculate the average of embeddings
+            averaged_embeddings = summed_embeddings / token_count
+        else:
+            # Convert the list of averaged embeddings to a tensor
+            averaged_embeddings = torch.mean(embeddings, dim=1)
 
         # Pass the embeddings through the MLP layer
-        final_out = self.network(average_embeddings)
+        final_out = self.network(averaged_embeddings.to(self.device))
 
         return final_out

src/embeddings/distilbert_layer.py
@@ -19,4 +19,5 @@ class DistilBertLayer(nn.Module):
         outputs = self.model(**encoded_inputs)[
             0
         ]  # Only retrieve the token embeddings, not the attention masks
-        return outputs
+        mask = None
+        return outputs, mask

src/embeddings/fasttext_layer.py
@@ -17,7 +17,11 @@ class FastTextLayer(nn.Module):
             for utterance in inputs
         ]
 
+        # Create a mask to track non-padding tokens
+        mask = [torch.ones(len(utterance.split())) for utterance in inputs]
+        mask = nn.utils.rnn.pad_sequence(mask, batch_first=True, padding_value=0)
+
         # Pad the sequences to the same length
         embeddings = nn.utils.rnn.pad_sequence(embeddings, batch_first=True)
 
-        return embeddings.to(self.device)
+        return embeddings.to(self.device), mask.to(self.device)